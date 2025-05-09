60ef4011a7467577cc6b276bd76707c1716c269b
tshzhu <tshzhu@gmail.com>
2025-05-09 15:20:10 +0800

    update cv

--- bibtex.js
+++ bibtex.js
@@ -11,15 +11,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 const isHidden = window.getComputedStyle(bibtexDiv).display === 'none';
                 bibtexDiv.style.display = isHidden ? 'block' : 'none';
 
-                // 展开时自动复制
-                if (isHidden) {
-                    const preContent = bibtexDiv.querySelector('.blockcontent pre');
-                    if (preContent) {
-                        const textToCopy = preContent.textContent;
-                        navigator.clipboard.writeText(textToCopy.trim())
-                            .catch(err => console.error('Fail to copy:', err));
-                    }
-                }
+                // // 展开时自动复制
+                // if (isHidden) {
+                //     const preContent = bibtexDiv.querySelector('.blockcontent pre');
+                //     if (preContent) {
+                //         const textToCopy = preContent.textContent;
+                //         navigator.clipboard.writeText(textToCopy.trim())
+                //             .catch(err => console.error('Fail to copy:', err));
+                //     }
+                // }
             }
         });
     });
